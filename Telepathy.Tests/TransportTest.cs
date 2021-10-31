faa1d801c027379a8681278493df179bad3c3ba9
##[s3t-3nv name=zanecop]Was here! <zanecop@github.com>
2021-10-31 11:54:44 -0700

    Update TransportTest.cs

--- Telepathy.Tests/TransportTest.cs
+++ Telepathy.Tests/TransportTest.cs
@@ -77,6 +77,7 @@ namespace Telepathy.Tests
             Client client = new Client(MaxMessageSize);
             for (int i = 0; i < 1000; i++)
             {
+                Thread.Sleep(10);
                 client.Connect("127.0.0.1", port);
                 Assert.That(client.Connecting || client.Connected, Is.True);
                 client.Disconnect();
